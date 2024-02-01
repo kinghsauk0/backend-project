72da31c5469e48f9005aab690e4e30bf07c1b03f
kinghsauk0 <kingshuk012345@gamil.com>
2024-02-01 21:49:31 +0530

    mongodb connect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import dotenv from "dotenv"
 
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path: ".env"
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running ${process.env.PORT}`)
+    })
+
+    app.on("error",(error)=>{
+        console.error("error", error)
+        throw error
+    })
+})
+.catch(err => console.error(`mongodb connect error: ${err}`))
 
 
 

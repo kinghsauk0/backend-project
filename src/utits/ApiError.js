aa7d126c47bbec750b7a5803f2c3b314aa987357
kinghsauk0 <kingshuk012345@gamil.com>
2024-02-03 20:10:15 +0530

    multer

--- src/utits/ApiError.js
+++ src/utits/ApiError.js
@@ -3,18 +3,18 @@ class ApiError extends Error{
         statusCode,
         message = "somethig is wrong",
         errors = [],
-        statck = false
+        stack = false
     ){
         super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.errors = errors;
-        this.statck = statck;
+        this.stack = stack;
         this.success =false
         this.data = null;
 
-        if(statck){
-            this.statck = statck
+        if(stack){
+            this.stack = stack
         }else{
             Error.captureStackTrace(this,this.constructor)
         }
